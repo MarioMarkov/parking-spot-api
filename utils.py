21503388361b73fccf3a9821cfb3ddd40b250185
MarioMarkov <mario11septemvri@gmail.com>
2023-08-08 22:12:30 +0200

    Torch Runtime

--- utils.py
+++ utils.py
@@ -6,15 +6,16 @@ from torchvision import transforms
 import xml.etree.ElementTree as ET
 import time
 import onnxruntime
-
-# device = (
-#     "cuda"
-#     if torch.cuda.is_available()
-#     else "cpu"
-#     if torch.backends.mps.is_available()
-#     else "cpu"
-# )
-# model_path = "m_alex_net.pth"
+import torch
+import torch.nn as nn
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "cpu"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+model_path = "m_alex_net.pth"
 
 
 def to_numpy(tensor):
@@ -26,58 +27,58 @@ def to_numpy(tensor):
 print("loading model...")
 
 
-# class mAlexNet(nn.Module):
-#     def __init__(self, num_classes=2):
-#         super().__init__()
-#         self.input_channel = 3
-#         self.num_output = num_classes
-#         self.layer1 = nn.Sequential(
-#             nn.Conv2d(
-#                 in_channels=self.input_channel,
-#                 out_channels=16,
-#                 kernel_size=11,
-#                 stride=4,
-#             ),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=3, stride=2),
-#         )
+class mAlexNet(nn.Module):
+    def __init__(self, num_classes=2):
+        super().__init__()
+        self.input_channel = 3
+        self.num_output = num_classes
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=self.input_channel,
+                out_channels=16,
+                kernel_size=11,
+                stride=4,
+            ),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
 
-#         self.layer2 = nn.Sequential(
-#             nn.Conv2d(in_channels=16, out_channels=20, kernel_size=5, stride=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=3, stride=2),
-#         )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=20, kernel_size=5, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
 
-#         self.layer3 = nn.Sequential(
-#             nn.Conv2d(in_channels=20, out_channels=30, kernel_size=3, stride=1),
-#             nn.ReLU(inplace=True),
-#             nn.MaxPool2d(kernel_size=3, stride=2),
-#         )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(in_channels=20, out_channels=30, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
 
-#         self.layer4 = nn.Sequential(
-#             nn.Linear(30 * 3 * 3, out_features=48), nn.ReLU(inplace=True)
-#         )
+        self.layer4 = nn.Sequential(
+            nn.Linear(30 * 3 * 3, out_features=48), nn.ReLU(inplace=True)
+        )
 
-#         self.layer5 = nn.Sequential(
-#             nn.Linear(in_features=48, out_features=self.num_output)
-#         )
+        self.layer5 = nn.Sequential(
+            nn.Linear(in_features=48, out_features=self.num_output)
+        )
 
-#     def forward(self, x):
-#         x = self.layer1(x)
-#         x = self.layer2(x)
-#         x = self.layer3(x)
-#         x = x.view(x.size(0), -1)
-#         x = self.layer4(x)
-#         logits = self.layer5(x)
-#         return logits
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = x.view(x.size(0), -1)
+        x = self.layer4(x)
+        logits = self.layer5(x)
+        return logits
 
 
-ort_session = onnxruntime.InferenceSession("onxx_malex_net.onnx")
+#ort_session = onnxruntime.InferenceSession("onxx_malex_net.onnx")
 
-# model = mAlexNet(num_classes=2).to(device)
-# model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
+model = mAlexNet(num_classes=2).to(device)
+model.load_state_dict(torch.load(model_path, map_location=torch.device(device)))
 
-# model.eval()
+model.eval()
 
 
 def extract_bndbox_values(tree):
@@ -138,17 +139,17 @@ def predict(image_path, xml_dir, require_parsing):
 
         img = transform(patch)
         img = img.unsqueeze(0)
-        # img = img.to(device)
-        # with torch.no_grad():
-        # outputs = model(img)
-        # print(outputs)
-        # _, preds = torch.max(outputs, 1)
-        ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(img)}
-        ort_outs = ort_session.run(None, ort_inputs)
-        img_out_y = ort_outs[0]
-        preds = list()
-        [preds.append(np.argmax(pred)) for pred in img_out_y]
-        is_busy = preds[0]
+        img = img.to(device)
+        with torch.no_grad():
+            outputs = model(img)
+            print(outputs)
+            _, preds = torch.max(outputs, 1)
+        # ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(img)}
+        # ort_outs = ort_session.run(None, ort_inputs)
+        # img_out_y = ort_outs[0]
+        # preds = list()
+        # [preds.append(np.argmax(pred)) for pred in img_out_y]
+            is_busy = preds[0]
 
         if is_busy == 1:
             # Busy 1 Red
